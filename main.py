db66f6bc20588a793cf5e69c638481e6b5bf5a48
Jonas <jonas.luft@hcu-hamburg.de>
2019-10-15 10:18:06 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import json
+import urllib.request 
+
+def getFromCfg(key : str) -> str:
+    #import os#os.path.dirname(os.path.realpath(__file__)+
+    with open("config.json") as file:
+        js = json.load(file)
+        return js[key]
+
+def getCurrentState():
+    with urllib.request.urlopen(getFromCfg("input_url")) as url:
+        return json.loads(url.read().decode())
+    return None
+
+class Grid:
+    cellSize = 0
+    ncols = 0
+    nrows = 0
+
+    @staticmethod
+    def fromCityIO(data):
+        ret = Grid()
+        ret.cellSize = data["header"]["spatial"]["cellSize"]
+        ret.ncols = data["header"]["spatial"]["ncols"]
+        ret.nrows = data["header"]["spatial"]["nrows"]
+        return ret
+
+
+
+if __name__ == "__main__":
+    data = getCurrentState()
+    if not data:
+        print("couldn't load input_url!")
+        exit()
+
+    gridDef = Grid.fromCityIO(data)
+    gridData = data["grid"]
+
+    if gridData:
+        print("got grid")
+
